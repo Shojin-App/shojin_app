8f3044c033f9977e020513e37163cb22a37f5a25
yuubinnkyoku <yuubinnkyoku@users.noreply.github.com>
2025-09-25 18:29:53 +0900

    fix: update Kotlin plugin version and add build features in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,6 +21,10 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
     applicationId = "io.github.shojinapp.kyopro"
