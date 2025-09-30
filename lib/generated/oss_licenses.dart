9815163c61596db2f60af297365c2b6f71f62880
yuubinnkyoku <yuubinnkyoku@users.noreply.github.com>
2025-09-30 19:41:05 +0900

    feat: Add file_picker and flutter_plugin_android_lifecycle packages

--- lib/generated/oss_licenses.dart
+++ lib/generated/oss_licenses.dart
@@ -40,6 +40,7 @@ const allDependencies = <Package>[
   _favicon,
   _ffi,
   _file,
+  _file_picker,
   _fixnum,
   _flutter,
   _flutter_code_editor,
@@ -53,6 +54,7 @@ const allDependencies = <Package>[
   _flutter_markdown_plus,
   _flutter_math_fork,
   _flutter_oss_licenses,
+  _flutter_plugin_android_lifecycle,
   _flutter_svg,
   _flutter_web_plugins,
   _google_fonts,
@@ -181,6 +183,7 @@ const dependencies = <Package>[
   _webview_flutter,
   _favicon,
   _share_plus,
+  _file_picker,
   _open_file,
   _intl,
   _animations,
@@ -2534,6 +2537,41 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
     devDependencies: [PackageRef('lints')],
   );
 
+/// file_picker 8.3.7
+const _file_picker = Package(
+    name: 'file_picker',
+    description: 'A package that allows you to use a native file explorer to pick single or multiple absolute file paths, with extension filtering support.',
+    homepage: 'https://github.com/miguelpruivo/plugins_flutter_file_picker',
+    repository: 'https://github.com/miguelpruivo/flutter_file_picker',
+    authors: [],
+    version: '8.3.7',
+    license: '''MIT License
+
+Copyright (c) 2018 Miguel Ruivo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.''',
+    isMarkdown: false,
+    isSdk: false,
+    dependencies: [PackageRef('flutter'), PackageRef('flutter_web_plugins'), PackageRef('flutter_plugin_android_lifecycle'), PackageRef('plugin_platform_interface'), PackageRef('ffi'), PackageRef('path'), PackageRef('win32'), PackageRef('cross_file'), PackageRef('web')],
+    devDependencies: [PackageRef('lints')],
+  );
+
 /// fixnum 1.1.1
 const _fixnum = Package(
     name: 'fixnum',
@@ -3399,6 +3437,44 @@ SOFTWARE.''',
     isSdk: false,
     dependencies: [PackageRef('dart_pubspec_licenses')],
     devDependencies: [PackageRef('flutter_lints')],
+  );
+
+/// flutter_plugin_android_lifecycle 2.0.30
+const _flutter_plugin_android_lifecycle = Package(
+    name: 'flutter_plugin_android_lifecycle',
+    description: 'Flutter plugin for accessing an Android Lifecycle within other plugins.',
+    repository: 'https://github.com/flutter/packages/tree/main/packages/flutter_plugin_android_lifecycle',
+    authors: [],
+    version: '2.0.30',
+    license: '''Copyright 2013 The Flutter Authors. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of Google Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
+    isMarkdown: false,
+    isSdk: false,
+    dependencies: [PackageRef('flutter')],
+    devDependencies: [],
   );
 
 /// flutter_svg 2.2.1
@@ -8303,7 +8379,7 @@ Public License instead of this License.  But first, please read
 <https://www.gnu.org/licenses/why-not-lgpl.html>.''',
     isMarkdown: false,
     isSdk: false,
-    dependencies: [PackageRef('flutter'), PackageRef('package_info_plus'), PackageRef('device_info_plus'), PackageRef('path_provider'), PackageRef('pub_semver'), PackageRef('url_launcher'), PackageRef('permission_handler'), PackageRef('archive'), PackageRef('crypto'), PackageRef('cupertino_icons'), PackageRef('dynamic_color'), PackageRef('google_fonts'), PackageRef('flutter_svg'), PackageRef('flutter_local_notifications'), PackageRef('timezone'), PackageRef('http'), PackageRef('html'), PackageRef('flutter_markdown_plus'), PackageRef('flutter_code_editor'), PackageRef('code_text_field'), PackageRef('highlight'), PackageRef('flutter_highlight'), PackageRef('shared_preferences'), PackageRef('provider'), PackageRef('webview_flutter'), PackageRef('favicon'), PackageRef('share_plus'), PackageRef('open_file'), PackageRef('intl'), PackageRef('animations'), PackageRef('yaml'), PackageRef('flutter_math_fork')],
+    dependencies: [PackageRef('flutter'), PackageRef('package_info_plus'), PackageRef('device_info_plus'), PackageRef('path_provider'), PackageRef('pub_semver'), PackageRef('url_launcher'), PackageRef('permission_handler'), PackageRef('archive'), PackageRef('crypto'), PackageRef('cupertino_icons'), PackageRef('dynamic_color'), PackageRef('google_fonts'), PackageRef('flutter_svg'), PackageRef('flutter_local_notifications'), PackageRef('timezone'), PackageRef('http'), PackageRef('html'), PackageRef('flutter_markdown_plus'), PackageRef('flutter_code_editor'), PackageRef('code_text_field'), PackageRef('highlight'), PackageRef('flutter_highlight'), PackageRef('shared_preferences'), PackageRef('provider'), PackageRef('webview_flutter'), PackageRef('favicon'), PackageRef('share_plus'), PackageRef('file_picker'), PackageRef('open_file'), PackageRef('intl'), PackageRef('animations'), PackageRef('yaml'), PackageRef('flutter_math_fork')],
     devDependencies: [PackageRef('flutter_launcher_icons'), PackageRef('flutter_lints'), PackageRef('change_app_package_name'), PackageRef('flutter_oss_licenses')],
   );
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import device_info_plus
 import dynamic_color
+import file_picker
 import flutter_local_notifications
 import open_file_mac
 import package_info_plus
@@ -19,6 +20,7 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
